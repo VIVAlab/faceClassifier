ae100d143e4dd2c09f1e5d1df3201db5417ce672
Binghao <wangbhsoon@gmail.com>
2016-02-22 21:44:39 -0500

    createCNNs done

--- src2/main.cpp
+++ src2/main.cpp
@@ -10,15 +10,55 @@ using namespace std;
 int main(int, char**)
 {
 
-        string filename = "/home/binghao/faceClassifier/preprocess/module.bin";
+        string filename = "/home/binghao/faceClassifier/weights/12net.bin";
         string ofilename = filename + ".xml";
-        cnn::CNN net("12cnet");
-        createCNN12(filename, net);
+        cnn::CNN net12("12net");
+        createCNN12(filename, net12);
+        FileStorage fs12(ofilename, FileStorage::WRITE);
+        fs12 << "cnn" <<  net12;
+        // cout << net12 << endl;
+        fs12.release();
 
-        FileStorage fs(ofilename, FileStorage::WRITE);
-        fs << "cnn" <<  net;
-        // cout << net <<endl;
-        fs.release();
+        filename = "/home/binghao/faceClassifier/weights/12cnet.bin";
+        ofilename = filename + ".xml";
+        cnn::CNN net12c("12cnet");
+        createCNN12Calibration(filename, net12c);
+        FileStorage fs12c(ofilename, FileStorage::WRITE);
+        fs12c << "cnn" << net12c;
+        fs12c.release();
+
+        filename = "/home/binghao/faceClassifier/weights/24net.bin";
+        ofilename = filename + ".xml";
+        cnn::CNN net24("24net");
+        createCNN24(filename, net24);
+        FileStorage fs24(ofilename, FileStorage::WRITE);
+        fs24 << "cnn" << net24;
+        fs24.release();
+
+        filename = "/home/binghao/faceClassifier/weights/24cnet.bin";
+        ofilename = filename + ".xml";
+        cnn::CNN net24c("24cnet");
+        createCNN24Calibration(filename, net24c);
+        FileStorage fs24c(ofilename, FileStorage::WRITE);
+        fs24c << "cnn" << net24c;
+        fs24c.release();
+
+        filename = "/home/binghao/faceClassifier/weights/48net.bin";
+        ofilename = filename + ".xml";
+        cnn::CNN net48("48net");
+        createCNN48(filename, net48);
+        FileStorage fs48(ofilename, FileStorage::WRITE);
+        fs48 << "cnn" << net48;
+        fs48.release();
+
+        filename = "/home/binghao/faceClassifier/weights/48cnet.bin";
+        ofilename = filename + ".xml";
+        cnn::CNN net48c("48cnet");
+        createCNN48Calibration(filename, net48c);
+        FileStorage fs48c(ofilename, FileStorage::WRITE);
+        fs48c << "cnn" << net48c;
+        fs48c.release();
+
 
 
     return 0;
--- src2/storage.h
+++ src2/storage.h
@@ -126,7 +126,7 @@ static void createCNN12(const string &filename, cnn::CNN &net)
     params.KernelD = 1;
     params.NLayers = 1;
 
-    createMAXPOOL(module2, params);
+    createMAXPOOL(module2, params);     // default parameters??
     net.addLayer(module2);
 
     createRELU(module3);
@@ -151,6 +151,327 @@ static void createCNN12(const string &filename, cnn::CNN &net)
 
     createSOFTMAX(module7);
     net.addLayer(module7);
+
+    f.close();
+}
+
+static void createCNN12Calibration(const string &filename, cnn::CNN &net)
+{
+    ifstream f(filename, ios::in | ios::binary);
+
+    cnn::CNNLayer module1, module2, module3, module4, module5, module6, module7;
+
+    CNNParam params;
+    params.PadH = 0;
+    params.PadW = 0;
+    params.StrideW = 1;
+    params.StrideH = 1;
+    params.KernelH = 3;
+    params.KernelW = 3;
+    params.KernelD = 1;
+    params.NLayers = 16;
+
+    createCONV(module1, params, f);
+    net.addLayer(module1);
+
+    createRELU(module2);
+    net.addLayer(module2);
+
+    params.PadH = 1;
+    params.PadW = 1;
+    params.StrideW = 2;
+    params.StrideH = 2;
+    params.KernelH = 3;
+    params.KernelW = 3;
+    params.KernelD = 1;
+    params.NLayers = 1;
+    createMAXPOOL(module3, params);
+    net.addLayer(module3);
+
+    params.KernelH = 5;
+    params.KernelW = 5;
+    params.KernelD = 16;
+    params.NLayers = 128;
+    createFC(module4, params, f);
+    net.addLayer(module4);
+
+    createRELU(module5);
+    net.addLayer(module5);
+
+    params.KernelH = 128;
+    params.KernelW = 1;
+    params.KernelD = 1;
+    params.NLayers = 45;
+    createFC(module6, params, f);
+    net.addLayer(module6);
+
+    createSOFTMAX(module7);
+    net.addLayer(module7);
+
+    f.close();
+}
+
+static void createCNN24(const string &filename, cnn::CNN &net)
+{
+    ifstream f(filename, ios::in | ios::binary);
+
+    cnn::CNNLayer module1, module2, module3, module4, module5, module6, module7;
+
+    CNNParam params;
+    params.PadH = 0;
+    params.PadW = 0;
+    params.StrideW = 1;
+    params.StrideH = 1;
+    params.KernelH = 5;
+    params.KernelW = 5;
+    params.KernelD = 1;
+    params.NLayers = 64;
+
+    createCONV(module1, params, f);
+    net.addLayer(module1);
+
+    createRELU(module2);
+    net.addLayer(module2);
+
+    params.PadH = 1;
+    params.PadW = 1;
+    params.StrideW = 2;
+    params.StrideH = 2;
+    params.KernelH = 3;
+    params.KernelW = 3;
+    params.KernelD = 1;
+    params.NLayers = 1;
+    createMAXPOOL(module3, params);
+    net.addLayer(module3);
+
+    params.KernelH = 10;
+    params.KernelW = 10;
+    params.KernelD = 64;
+    params.NLayers = 128;
+    createFC(module4, params, f);
+    net.addLayer(module4);
+
+    createRELU(module5);
+    net.addLayer(module5);
+
+    params.KernelH = 128;
+    params.KernelW = 1;
+    params.KernelD = 1;
+    params.NLayers = 2;
+    createFC(module6, params, f);
+    net.addLayer(module6);
+
+    createSOFTMAX(module7);
+    net.addLayer(module7);
+
+    f.close();
+}
+
+static void createCNN24Calibration(const string &filename, cnn::CNN &net)
+{
+    ifstream f(filename, ios::in | ios::binary);
+
+    cnn::CNNLayer module1, module2, module3, module4, module5, module6, module7;
+
+    CNNParam params;
+    params.PadH = 0;
+    params.PadW = 0;
+    params.StrideW = 1;
+    params.StrideH = 1;
+    params.KernelH = 5;
+    params.KernelW = 5;
+    params.KernelD = 1;
+    params.NLayers = 32;
+
+    createCONV(module1, params, f);
+    net.addLayer(module1);
+
+    params.PadH = 1;
+    params.PadW = 1;
+    params.StrideW = 2;
+    params.StrideH = 2;
+    params.KernelH = 3;
+    params.KernelW = 3;
+    params.KernelD = 1;
+    params.NLayers = 1;
+    createMAXPOOL(module2, params);
+    net.addLayer(module2);
+
+    createRELU(module3);
+    net.addLayer(module3);
+
+    params.KernelH = 10;
+    params.KernelW = 10;
+    params.KernelD = 32;
+    params.NLayers = 64;
+    createFC(module4, params, f);
+    net.addLayer(module4);
+
+    createRELU(module5);
+    net.addLayer(module5);
+
+    params.KernelH = 64;
+    params.KernelW = 1;
+    params.KernelD = 1;
+    params.NLayers = 45;
+    createFC(module6, params, f);
+    net.addLayer(module6);
+
+    createSOFTMAX(module7);
+    net.addLayer(module7);
+
+    f.close();
+}
+
+static void createCNN48(const string &filename, cnn::CNN &net)
+{
+    ifstream f(filename, ios::in | ios::binary);
+
+    cnn::CNNLayer module1, module2, module3, module4, module5, module6, module7, module8, module9, module10;
+
+    CNNParam params;
+    params.PadH = 0;
+    params.PadW = 0;
+    params.StrideW = 1;
+    params.StrideH = 1;
+    params.KernelH = 5;
+    params.KernelW = 5;
+    params.KernelD = 1;
+    params.NLayers = 64;
+
+    createCONV(module1, params, f);
+    net.addLayer(module1);
+
+    params.PadH = 1;
+    params.PadW = 1;
+    params.StrideW = 2;
+    params.StrideH = 2;
+    params.KernelH = 3;
+    params.KernelW = 3;
+    params.KernelD = 1;
+    params.NLayers = 1;
+    createMAXPOOL(module3, params);
+    net.addLayer(module3);
+
+    createRELU(module3);
+    net.addLayer(module3);
+
+    params.PadH = 0;
+    params.PadW = 0;
+    params.StrideW = 1;
+    params.StrideH = 1;
+    params.KernelH = 5;
+    params.KernelW = 5;
+    params.KernelD = 64;
+    params.NLayers = 64;
+
+    createCONV(module4, params, f);
+    net.addLayer(module4);
+
+    params.PadH = 1;
+    params.PadW = 1;
+    params.StrideW = 2;
+    params.StrideH = 2;
+    params.KernelH = 3;
+    params.KernelW = 3;
+    params.KernelD = 1;
+    params.NLayers = 1;
+    createMAXPOOL(module5, params);
+    net.addLayer(module5);
+
+    createRELU(module6);
+    net.addLayer(module6);
+
+    params.KernelH = 9;
+    params.KernelW = 9;
+    params.KernelD = 64;
+    params.NLayers = 256;
+    createFC(module7, params, f);
+    net.addLayer(module7);
+
+    createRELU(module8);
+    net.addLayer(module8);
+
+    params.KernelH = 256;
+    params.KernelW = 1;
+    params.KernelD = 1;
+    params.NLayers = 2;
+    createFC(module9, params, f);
+    net.addLayer(module9);
+
+    createSOFTMAX(module10);
+    net.addLayer(module10);
+
+    f.close();
+}
+
+static void createCNN48Calibration(const string &filename, cnn::CNN &net)
+{
+    ifstream f(filename, ios::in | ios::binary);
+
+    cnn::CNNLayer module1, module2, module3, module4, module5, module6, module7, module8, module9;
+
+    CNNParam params;
+    params.PadH = 0;
+    params.PadW = 0;
+    params.StrideW = 1;
+    params.StrideH = 1;
+    params.KernelH = 5;
+    params.KernelW = 5;
+    params.KernelD = 1;
+    params.NLayers = 64;
+
+    createCONV(module1, params, f);
+    net.addLayer(module1);
+
+    params.PadH = 1;
+    params.PadW = 1;
+    params.StrideW = 2;
+    params.StrideH = 2;
+    params.KernelH = 3;
+    params.KernelW = 3;
+    params.KernelD = 1;
+    params.NLayers = 1;
+    createMAXPOOL(module2, params);
+    net.addLayer(module2);
+
+    createRELU(module3);
+    net.addLayer(module3);
+
+    params.PadH = 0;
+    params.PadW = 0;
+    params.StrideW = 1;
+    params.StrideH = 1;
+    params.KernelH = 5;
+    params.KernelW = 5;
+    params.KernelD = 64;
+    params.NLayers = 64;
+
+    createCONV(module4, params, f);
+    net.addLayer(module4);
+
+    createRELU(module5);
+    net.addLayer(module5);
+
+    params.KernelH = 18;
+    params.KernelW = 18;
+    params.KernelD = 64;
+    params.NLayers = 256;
+    createFC(module6, params, f);
+    net.addLayer(module6);
+
+    createRELU(module7);
+    net.addLayer(module7);
+
+    params.KernelH = 256;
+    params.KernelW = 1;
+    params.KernelD = 1;
+    params.NLayers = 45;
+    createFC(module8, params, f);
+    net.addLayer(module8);
+
+    createSOFTMAX(module9);
+    net.addLayer(module9);
 
     f.close();
 }
